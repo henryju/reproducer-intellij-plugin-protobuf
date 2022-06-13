620a34623ef698bc8e9ee397c35e878745c00658
GitHub Action <action@github.com>
2022-06-13 15:39:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/henryju/reproducerintellijpluginprotobuf/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/henryju/reproducerintellijpluginprotobuf/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.henryju.reproducerintellijpluginprotobuf.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.henryju.reproducerintellijpluginprotobuf.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/henryju/reproducerintellijpluginprotobuf/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.henryju.reproducerintellijpluginprotobuf.services
+
+import com.github.henryju.reproducerintellijpluginprotobuf.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
